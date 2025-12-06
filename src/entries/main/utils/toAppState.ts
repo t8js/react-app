5bf5ebc6d713fbb1f469a0cc3836ee41a7918839
axtk <axtkx@inbox.ru>
2025-12-06 21:48:16 +0300

    update deps

--- src/entries/main/utils/toAppState.ts
+++ src/entries/main/utils/toAppState.ts
@@ -1,6 +1,6 @@
-import { persist, Store } from "@t8/react-store";
+import { PersistentStore } from "@t8/react-store";
 import type { SerializableAppState } from "../types/SerializableAppState.ts";
 
 export function toAppState(state: SerializableAppState) {
-  return persist(new Store(state), "react-app.store");
+  return new PersistentStore(state, "react-app.store");
 }
